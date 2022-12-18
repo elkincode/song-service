5d83f3a9b0f15bf488c51f3c23213403223c3a7c
elkin <elk.iv@ya.ru>
2022-12-19 00:00:05 +0300

    Tests

---
+++ tests/test_main.py
@@ -0,0 +1,21 @@
+import requests
+
+api = 'http://localhost:8081/song'
+
+
+def test_song_empty_get():
+    s = requests.Session()
+    id = '8c34c25b-b4a1-477a-a938-d6ad10442c98'
+    response = s.get(f'{api}/{id}')
+    assert response.status_code == 200
+
+def test_song_save_and_get():
+    s = requests.Session()
+    parameters = "?title=Daydreamer&artistId=8c34c25b-b4a1-477a-a938-d6ad10442c98"
+    response = s.post(f'{api}/{parameters}')
+    assert response.status_code == 200
+    assert response.json().get('title') == 'Daydreamer'
+    createdSongId = response.json().get('id')
+    responseSong = s.get(f'{api}/{createdSongId}')
+    assert responseSong.status_code == 200
+    assert responseSong.json().get('title') == 'Daydreamer'

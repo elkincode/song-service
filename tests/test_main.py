e69e9ee380b143b453e35c48659a6ad89b97d179
elkin <elk.iv@ya.ru>
2022-12-19 19:11:23 +0300

    Wreck It

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,12 +6,12 @@ api = 'http://localhost:8081/song'
 def test_song_empty_get():
     s = requests.Session()
     id = '8c34c25b-b4a1-477a-a938-d6ad10442c98'
-    response = s.get(f'{api}/{id}')
+    response = s.get(f'{api}ahaha/{id}')
     assert response.status_code == 200
 
 def test_song_save_and_get():
     s = requests.Session()
-    parameters = "?title=Daydreamer&artistId=8c34c25b-b4a1-477a-a938-d6ad10442c98"
+    parameters = "?title=Daydreamer&broken=8c34c25b-b4a1-477a-a938-d6ad10442c98"
     response = s.post(f'{api}/{parameters}')
     assert response.status_code == 200
     assert response.json().get('title') == 'Daydreamer'
